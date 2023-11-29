43148c953623b7df5dbf4f15d83e06823fe30d8c
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2023-11-29 01:40:54 -0700

    Add method to run all days at once, currently set up to run Day 1.

---
+++ src/me/madcabbage/aoc/Main.java
@@ -0,0 +1,13 @@
+package me.madcabbage.aoc;
+
+import me.madcabbage.aoc.aoc2022.day1.Day1;
+
+public class Main {
+    public static void main(String[] args) {
+        runAllDays();
+    }
+
+    private static void runAllDays() {
+        new Day1().run();
+    }
+}
