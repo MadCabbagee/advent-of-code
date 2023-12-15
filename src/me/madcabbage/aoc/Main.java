6464da9506b1dd2d379493f0d8442307ab3011d7
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2023-12-15 15:48:39 -0700

    add main method

--- src/me/madcabbage/aoc/Main.java
+++ src/me/madcabbage/aoc/Main.java
@@ -1,13 +1,69 @@
 package me.madcabbage.aoc;
 
-import me.madcabbage.aoc.aoc2022.day1.Day1;
+import me.madcabbage.aoc.aoc2022.AOC2022;
+import me.madcabbage.aoc.aoc2023.AOC2023;
 
 public class Main {
+    public static void main(String[] args) {
+        runAllYears();
+    }
+
+    private static void runAllYears() {
+        new AOC2022().runAll();
+        new AOC2023().runAll();
+    }
+}
+
+/*public class Main {
+
+    private static final List<AbstractDay> days = new ArrayList<>();
     public static void main(String[] args) {
         runAllDays();
     }
 
     private static void runAllDays() {
-        new Day1().run();
+        for (var day : days) {
+            day.run();
+        }
     }
-}
+
+    public static void addDay(AbstractDay day) {
+        days.add(day);
+    }
+
+}*/
+
+/*public class Main {
+
+    private static Main instance;
+    private final List<AbstractDay> days = new ArrayList<>();
+
+    private Main() {
+        if (instance != null) {
+            throw new IllegalStateException("Singleton instance already created. Use getInstance() method.");
+        }
+        instance = this;
+    }
+
+    public static void main(String[] args) {
+        getInstance().runAllDays();
+    }
+
+    private void runAllDays() {
+        for (var day : days) {
+            day.run();
+        }
+    }
+
+    public void addDay(AbstractDay day) {
+        days.add(day);
+    }
+
+    public static Main getInstance() {
+        if (instance == null) {
+            instance = new Main();
+        }
+        return instance;
+    }
+
+}*/
