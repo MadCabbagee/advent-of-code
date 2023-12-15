08e066d637ce7f71e0f968c6202941aec743d418
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2023-12-15 11:12:55 -0700

    remove generics, remove abstract run, add reference to 'parent' day, add getter and setter for 'parent' day.

--- src/me/madcabbage/aoc/AbstractPart.java
+++ src/me/madcabbage/aoc/AbstractPart.java
@@ -1,10 +1,26 @@
 package me.madcabbage.aoc;
 
-public abstract class AbstractPart<T> implements IPart<T> {
+public abstract class AbstractPart implements IPart {
+
+    protected AbstractDay day;
 
     protected final void printAnswer(String answer) {
         System.out.println("\t\tanswer: " + answer);
     }
+
     @Override
-    public abstract void run(T parsedInput);
+    public AbstractDay getDay() {
+        if (day == null) {
+            System.out.println("get day: " + day);
+        }
+        return this.day;
+    }
+
+    @Override
+    public void setDay(AbstractDay day) {
+        if (day == null) {
+            System.out.println("set day: " + day);
+        }
+        this.day = day;
+    }
 }
