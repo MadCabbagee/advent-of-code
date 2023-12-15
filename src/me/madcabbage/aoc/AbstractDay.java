e658d0ff9f47ee0c47735e93c6b7850b9bfed477
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2023-12-15 11:10:09 -0700

    remove generics, add year, modify constructor, set day on part objects, time and print execution of each part, rename parseInput to readInput and implement it.

--- src/me/madcabbage/aoc/AbstractDay.java
+++ src/me/madcabbage/aoc/AbstractDay.java
@@ -1,32 +1,84 @@
 package me.madcabbage.aoc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-public abstract class AbstractDay<T> implements IDay {
+public abstract class AbstractDay implements IDay {
 
+    protected final short year;
     protected final byte day;
-    protected final IPart<T> partOne;
-    protected final IPart<T> partTwo;
+    protected final IPart partOne;
+    protected final IPart partTwo;
     protected final Path inputPath;
 
-    protected AbstractDay(byte day, IPart<T> partOne, IPart<T> partTwo) {
+    protected AbstractDay(short year, byte day, IPart partOne, IPart partTwo) {
+        this.year = year;
         this.day = day;
         this.partOne = partOne;
+        this.partOne.setDay(this);
+
         this.partTwo = partTwo;
-        this.inputPath = Paths.get("day" + day, "input.txt");
+        this.partTwo.setDay(this);
+
+        this.inputPath = Paths.get(String.valueOf(year) ,"day" + day, "input.txt");
+
+        //Main.addDay(this);
     }
 
     @Override
     public void run() {
-        T parsedInput = parseInput();
+        List<String> inputLines = readInput();
+
         System.out.println("Day " + day + ":");
         System.out.println("\tPart 1:");
-        partOne.run(parsedInput);
+
+        long p1StartTime = System.currentTimeMillis();
+        partOne.run(inputLines);
+        long p1StopTime = System.currentTimeMillis();
+
+        long p1RunTime = (p1StopTime - p1StartTime);
+        System.out.println("\t\tRuntime: " + p1RunTime + "ms");
+
         System.out.println("\tPart 2:");
-        partTwo.run(parsedInput);
-        System.out.println();
+        long p2StartTime = System.currentTimeMillis();
+        partTwo.run(inputLines);
+        long p2StopTime = System.currentTimeMillis();
+
+        long p2RunTime = (p2StopTime - p2StartTime);
+        System.out.println("\t\tRuntime: " + p2RunTime + "ms");
+
+        System.out.println("\tTotal runtime: " + (p1RunTime + p2RunTime) + "ms");
+
+        System.out.println(/* blank for spacing */);
     }
 
-    protected abstract T parseInput();
+    protected List<String> readInput() {
+        List<String> inputLines = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(Objects.requireNonNull(AbstractDay.class.getClassLoader().getResourceAsStream(inputPath.toString()))))){
+            String line;
+            while ((line = br.readLine()) != null) {
+                inputLines.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return inputLines;
+    }
+
+    @Override
+    public IPart getPartOne() {
+        return partOne;
+    }
+
+    @Override
+    public IPart getPartTwo() {
+        return partTwo;
+    }
 }
