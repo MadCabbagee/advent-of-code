2d14e98ae3b3a9d12e212a8a6f383949f21cf2a8
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2023-12-15 13:21:18 -0700

    create default constructor calling super with year, day, and part instances.

---
+++ src/me/madcabbage/aoc/aoc2023/day2/Day2.java
@@ -0,0 +1,11 @@
+package me.madcabbage.aoc.aoc2023.day2;
+
+import me.madcabbage.aoc.AbstractDay;
+import me.madcabbage.aoc.aoc2023.day2.part1.Day2Part1;
+import me.madcabbage.aoc.aoc2023.day2.part2.Day2Part2;
+
+public class Day2 extends AbstractDay {
+    public Day2() {
+        super((short) 2023, (byte) 2, new Day2Part1(), new Day2Part2());
+    }
+}
