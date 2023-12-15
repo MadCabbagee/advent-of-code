3534c7d8c46b77081be895e2028e78a533a3b587
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2023-12-15 12:08:11 -0700

    cleanup imports, update run() method signature, implement solution

--- src/me/madcabbage/aoc/aoc2022/day1/part2/Day1Part2.java
+++ src/me/madcabbage/aoc/aoc2022/day1/part2/Day1Part2.java
@@ -1,22 +1,27 @@
 package me.madcabbage.aoc.aoc2022.day1.part2;
 
 import me.madcabbage.aoc.AbstractPart;
+import me.madcabbage.aoc.aoc2022.day1.Day1;
+import me.madcabbage.aoc.aoc2022.day1.exceptions.InvalidCalorieException;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Day1Part2 extends AbstractPart<List<Integer>> {
+public class Day1Part2 extends AbstractPart {
     @Override
-    public void run(List<Integer> parsedInput) {
-        int largestSum = 0;
+    public void run(List<String> input) {
+        List<Integer> integerInput = Day1.sumCalorieGroups(input);
+        // sum of top 3 largest calorie sums
+        int calorieSum = 0;
         for (int i = 0; i < 3; i++ ) {
             // get largest
-            int largest = Collections.max(parsedInput);
+            int largest = Collections.max(integerInput);
             // store largest
-            largestSum += largest;
+            calorieSum += largest;
             // remove largest
-            parsedInput.remove((Integer)largest);
+            integerInput.remove((Integer)largest);
         }
-        printAnswer(String.valueOf(largestSum));
+        printAnswer(String.valueOf(calorieSum));
     }
 }
