ed1302d8a9a0265cbd3857c3fdca17d9039e8c3f
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2023-12-15 14:32:14 -0700

    custom exception for invalid calories

---
+++ src/me/madcabbage/aoc/aoc2022/day1/exceptions/InvalidCalorieException.java
@@ -0,0 +1,7 @@
+package me.madcabbage.aoc.aoc2022.day1.exceptions;
+
+public class InvalidCalorieException extends RuntimeException {
+    public InvalidCalorieException(String message) {
+        super(message);
+    }
+}
