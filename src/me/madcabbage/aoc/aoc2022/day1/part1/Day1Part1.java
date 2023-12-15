14f2557e3d08ef5061fc2dfbf2a2215eda21e37c
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2023-12-15 12:07:46 -0700

    cleanup imports, update run() method signature, implement solution

--- src/me/madcabbage/aoc/aoc2022/day1/part1/Day1Part1.java
+++ src/me/madcabbage/aoc/aoc2022/day1/part1/Day1Part1.java
@@ -1,15 +1,18 @@
 package me.madcabbage.aoc.aoc2022.day1.part1;
 
 import me.madcabbage.aoc.AbstractPart;
+import me.madcabbage.aoc.aoc2022.day1.Day1;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Day1Part1 extends AbstractPart<List<Integer>> {
+public class Day1Part1 extends AbstractPart {
     @Override
-    public void run(List<Integer> parsedInput) {
+    public void run(List<String> input) {
+        List<Integer> integerInput = Day1.sumCalorieGroups(input);
         // What is the total number of Calories carried by the Elf with the highest calorie count?
-        int largest = Collections.max(parsedInput);
+        int largest = Collections.max(integerInput);
         printAnswer(String.valueOf(largest));
     }
 }
