aa886195eaabd32971dc0b9bd9216d5f9730a481
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2023-12-15 11:15:24 -0700

    cleanup imports, move custom exception to dedicated file, add year to constructor, catch NumberFormatException.

--- src/me/madcabbage/aoc/aoc2022/day1/Day1.java
+++ src/me/madcabbage/aoc/aoc2022/day1/Day1.java
@@ -1,50 +1,41 @@
 package me.madcabbage.aoc.aoc2022.day1;
 
 import me.madcabbage.aoc.AbstractDay;
+import me.madcabbage.aoc.Main;
+import me.madcabbage.aoc.aoc2022.day1.exceptions.InvalidCalorieException;
 import me.madcabbage.aoc.aoc2022.day1.part1.Day1Part1;
 import me.madcabbage.aoc.aoc2022.day1.part2.Day1Part2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-class InvalidCalorieException extends RuntimeException {
-    public InvalidCalorieException(String message) {
-        super(message);
-    }
-}
-public class Day1 extends AbstractDay<List<Integer>> {
+public class Day1 extends AbstractDay {
     public Day1() {
-        super((byte) 1, new Day1Part1(), new Day1Part2());
-    }
-
-    @Override
-    protected List<Integer> parseInput() {
-        BufferedReader br = new BufferedReader(new InputStreamReader(Objects.requireNonNull(Day1.class.getClassLoader().getResourceAsStream(inputPath.toString()))));
-        List<Integer> calorieSums = new ArrayList<>();
-        int tempSum = 0;
-        try {
-            String line;
-            while ((line = br.readLine()) != null) {
-                if (line.isEmpty()) {
-                    calorieSums.add(tempSum);
-                    tempSum = 0;
-                } else {
-                    tempSum += Integer.parseInt(line);
-                }
-            }
-            br.close();
-        } catch (IOException | NumberFormatException e) {
-            throw new InvalidCalorieException("Calorie could not be parsed");
-        }
-        return calorieSums;
+        super((short) 2022, (byte) 1, new Day1Part1(), new Day1Part2());
     }
 
     public static void main(String[] args) {
         new Day1().run();
+    }
+
+    public static List<Integer> sumCalorieGroups(List<String> input) {
+        List<Integer> calorieSums = new ArrayList<>();
+        int tempSum = 0;
+
+        for (String line : input) {
+            if (line.isEmpty()) {
+                calorieSums.add(tempSum);
+                tempSum = 0;
+            } else {
+                try {
+                    tempSum += Integer.parseInt(line);
+                } catch (NumberFormatException e) {
+                    throw new InvalidCalorieException("Error parsing calories into integers.");
+                }
+            }
+        }
+
+        return calorieSums;
     }
 
 }
